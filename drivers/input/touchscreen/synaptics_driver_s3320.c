821b8dfb0503d1aa82a1ca9c21207d36d080b111
lyq1996 <lyq123233@gmail.com>
2020-03-05 14:18:44 +0800

    driver: synaptics_s3320 fix gesture

--- drivers/input/touchscreen/synaptics_driver_s3320.c
+++ drivers/input/touchscreen/synaptics_driver_s3320.c
@@ -1205,8 +1205,8 @@ static void gesture_judge(struct synaptics_ts_data *ts)
 		(gesture_buffer[2] == 0x73) ? Sgestrue : UnkownGestrue;
 	}
 
-	keyCode = UnkownGestrue;
-	/* Get key code based on registered gesture */
+/*	keyCode = UnkownGestrue;	// Get key code based on registered gesture 
+
 	switch (gesture) {
 	case DouTap:
 		keyCode = KEY_DOUBLE_TAP;
@@ -1241,6 +1241,7 @@ static void gesture_judge(struct synaptics_ts_data *ts)
 	default:
 		break;
 	}
+*/
 
 	TPD_ERR("detect %s gesture\n", gesture == DouTap ? "(double tap)" :
 			gesture == UpVee ? "(V)" :
