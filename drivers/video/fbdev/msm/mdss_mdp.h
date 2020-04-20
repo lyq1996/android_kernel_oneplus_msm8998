137044a7ae0782f8be8a41d24dad550c157d81b5
shxyke <shxyke@gmail.com>
2020-04-20 15:44:47 +0800

    mdss: Implement PCC combining for LiveDisplay
    * If we send flicker free data via ioctl, we want to make sure
       it's preserved when applying RGB offsets via LiveDisplay.
     * Add support for combining the values, using the flicker
       free PCC value as a scaling factor.
    
    Signed-off-by: shxyke <shxyke@gmail.com>

--- drivers/video/fbdev/msm/mdss_mdp.h
+++ drivers/video/fbdev/msm/mdss_mdp.h
@@ -1853,6 +1853,8 @@ int mdss_mdp_pa_config(struct msm_fb_data_type *mfd,
 			struct mdp_pa_cfg_data *config, u32 *copyback);
 int mdss_mdp_pa_v2_config(struct msm_fb_data_type *mfd,
 			struct mdp_pa_v2_cfg_data *config, u32 *copyback);
+int mdss_mdp_kernel_pcc_config(struct msm_fb_data_type *mfd,
+			struct mdp_pcc_cfg_data *cfg_ptr, u32 *copyback);
 int mdss_mdp_pcc_config(struct msm_fb_data_type *mfd,
 			struct mdp_pcc_cfg_data *cfg_ptr, u32 *copyback);
 int mdss_mdp_igc_lut_config(struct msm_fb_data_type *mfd,
